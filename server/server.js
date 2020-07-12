4682a5c8db658c4914677b5b7053f9ac38eeaf88
Alexa <48775628+alexalpz@users.noreply.github.com>
2020-07-12 01:12:49 -0400

    Add files via upload

--- server/server.js
+++ server/server.js
@@ -6,13 +6,14 @@ var io = require('socket.io')(http);
 
 var mongoose = require('mongoose');
 
+
 app.use(express.static('../static'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:false}));
 
 
 var dbURL = "mongodb+srv://user:user@practicedb.pdomq.mongodb.net/practiceDB?retryWrites=true&w=majority";
-
+var chatURL = "mongodb+srv://user:user@chatmessages.lnikr.mongodb.net/chatMessages?retryWrites=true&w=majority";
 
 var Message = mongoose.model('message', {
     fname: String,
@@ -21,6 +22,10 @@ var Message = mongoose.model('message', {
     subject: String
 })
 
+var chatMessage = mongoose.model('chatmessage', {
+    name: String,
+    chatsubject: String
+})
 
 
 app.get('/messages',(req,res) =>{
@@ -29,10 +34,10 @@ app.get('/messages',(req,res) =>{
     })
     
 });
+
 app.post('/messages',(req, res) =>{
 
     var message = new Message(req.body);
-
     message.save((err) =>{
         if(err)
             sendStatus(500);
@@ -43,12 +48,47 @@ app.post('/messages',(req, res) =>{
 
 });
 
+
+
+app.post('/chatMessages',(req, res) =>{
+
+    var chatmessage = new chatMessage(req.body);
+
+    chatmessage.save((err) =>{
+        if(err)
+            sendStatus(500);
+       
+        io.emit('chatmessage', req.body);
+        res.sendStatus(200);
+    });
+
+});
+
+app.post('/chatMessages',(req, res) =>{
+
+    var chatmessage = new chatMessage(req.body);
+    chatmessage.save((err) =>{
+        if(err)
+            sendStatus(500);
+       
+        io.emit('chatmessage', req.body);
+        res.sendStatus(200);
+    });
+
+});
+
+
 io.on('connection',(socket) =>{
     console.log('A user connected');
 });
 
+
 mongoose.connect(dbURL,{ useMongoClient: true }, (err) =>{
     console.log("Mongo DB Connection...", err);
+});
+
+mongoose.connect(chatURL,{ useMongoClient: true }, (err) =>{
+    console.log("Chat DB Connection...", err);
 });
 
 
