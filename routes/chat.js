767316253876b5812592c0fb6015d5ab145e23a7
alexalpz <alexalopezit@gmail.com>
2020-07-15 23:12:00 -0400

    commit to NavigationEJS

--- routes/chat.js
+++ routes/chat.js
@@ -2,13 +2,16 @@ const express = require('express');
 const router = express.Router();
 
 module.exports = params => {
+    
     const {
         chatService
     } = params;
 
     router.get('/', async (req, res) => {
+       
         const chat = await chatService.getList();
         return res.json(chat);
+
     });
 
     router.post('/', (req, res) => {
